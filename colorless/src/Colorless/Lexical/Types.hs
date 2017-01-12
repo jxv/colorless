fadc92c591aaceda0264e24096cca27753f697d6
jxv <joevargas92@gmail.com>
2017-01-11 21:26:49 -0800

    lexical tokens

--- colorless/src/Colorless/Lexical/Types.hs
+++ colorless/src/Colorless/Lexical/Types.hs
@@ -1,5 +1,45 @@
 module Colorless.Lexical.Types
-  (
+  ( Line(..)
+  , Column(..)
+  , TokenVal(..)
+  , Token(..)
   ) where
 
 import Pregame
+
+newtype Line = Line Integer
+  deriving (Show, Eq, Num, Enum, Real, Ord, Integral)
+
+newtype Column = Column Integer
+  deriving (Show, Eq, Num, Enum, Real, Ord, Integral)
+
+data Loc = Loc
+  { _line :: Line
+  , _column :: Column
+  } deriving (Show, Eq)
+
+data TokenVal = TokenVal
+  { _loc :: Loc
+  , _val :: Token
+  } deriving (Show, Eq)
+
+data Token
+  = TokenLowerCamelCase Text
+  | TokenUpperCamelCase Text
+  | TokenNumber Integer
+  | TokenNest
+  | TokenPeriod
+  | TokenMinus
+  | TokenNewline
+  | TokenOpenParen
+  | TokenCloseParen
+  | TokenColon
+  | TokenEar
+  | TokenPercent
+  | TokenLeftAngle
+  | TokenRightAngle
+  | TokenBang
+  | TokenDollar
+  | TokenForwardSlash
+  | TokenPound
+  deriving (Show, Eq)
