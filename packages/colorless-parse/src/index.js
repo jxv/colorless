f36cd54243c6164c9c46f4b1f28e354279eceb71
jxv <joevargas92@gmail.com>
2017-02-14 07:13:09 -0800

    add description and validateFunction

--- packages/colorless-parse/src/index.js
+++ packages/colorless-parse/src/index.js
@@ -1,4 +1,5 @@
 /*
+ * _d = description
  * _n = name
  * _f = fields
  * _c = constructors
@@ -29,15 +30,16 @@ function isSum(v) {
     return hasKeys(['_n', '_c'], v) && notKeys(['_f', '_a', '_s', '_o'], v);
 }
 
-function Sum(name, params, ctors, tags) {
+function Sum(name, params, ctors, tags, description) {
     this._n = name;
     this._p = params || [];
     this._c = ctors;
     this._t = tags || [];
+    this._d = description || '';
 }
 
 function toSum(v) {
-    return new Sum(v._n, v._p, v._c, v._t);
+    return new Sum(v._n, v._p, v._c, v._t, v._d);
 }
 
 function isConstructor(v) {
@@ -57,30 +59,32 @@ function isProduct(v) {
     return hasKeys(['_n', '_f'], v) && notKeys(['_c', '_a', '_s', '_o'], v);
 }
 
-function Product(name, params, fields, tags) {
+function Product(name, params, fields, tags, description) {
     this._n = name;
     this._p = params || [];
     this._f = fields;
     this._t = tags || [];
+    this._d = description || '';
 }
 
 function toProduct(v) {
-    return new Product(v._n, v._p, v._f, v._t);
+    return new Product(v._n, v._p, v._f, v._t, v._d);
 }
 
 function isSynonym(v) {
     return hasKeys(['_n', '_s'], v) && notKeys(['_c', '_f', '_a', '_o'], v);
 }
 
-function Synonym(name, params, synonym, tags) {
+function Synonym(name, params, synonym, tags, description) {
     this._n = name;
     this._p = params || [];
     this._s = synonym;
     this._t = tags || [];
+    this._d = description || '';
 }
 
 function toSynonym(v) {
-    return new Synonym(v._n, v._p, v._s, v._t);
+    return new Synonym(v._n, v._p, v._s, v._t, v._d);
 }
 
 var types = {
@@ -140,9 +144,53 @@ function isFunctionDecl(f) {
 }
 
 function areFunctionDecls(v) {
-    return v._functions.map(function (f) { return [ f._n, isFunctionDecl(f) ]; } );
+    return v._functions.map(function (f) { return [f._n, isFunctionDecl(f)]; } );
+}
+
+function hasCamelCaseCharacters(s) {
+    for (var i in s) {
+        if (!((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' &&  s[i] <= '9'))) return false;
+    }
+    return true;
+
+}
+
+function isLowerCamelCase(s) {
+    if (!s.length) return false;
+    if (!(s[0] >= 'a' && s[0] <= 'z')) return false;
+    return hasCamelCaseCharacters(s);
+}
+
+function validateFunction(f) {
+    var errors = [];
+    if ('_n' in f) {
+        if (!(isLowerCamelCase(f._n))) {
+            errors.push('name must be lower camel case');
+        }
+    } else {
+        errors.push('missing name');
+    }
+    if ('_o' in f) {
+    } else {
+        errors.push('missing output');
+    }
+    if ('_a' in f) {
+    } else {
+        errors.push('missing arguments');
+    }
+    if ('_t' in f) {
+    } else {
+        errors.push('missing tags');
+    }
+    if ('_d' in f) {
+    } else {
+        errors.push('missing description');
+    }
+    return errors.length ? ['error', errors] : ['ok', f];
 }
 
 module.exports = {
-    parse: () => ""
+    parse: () => "",
+    validateFunction: validateFunction,
+    Error: Error
 };
--- packages/colorless-parse/test/parseSpec.js
+++ packages/colorless-parse/test/parseSpec.js
@@ -1,6 +1,20 @@
 const expect = require('chai').expect;
 
-describe("parsing", () => {
-    it("simple function ", () => {
+//const index = require('../src/index.js');
+const { validateFunction, Error,  Ok } = require('../src/index.js');
+
+describe("parsing validation", () => {
+    const hello = { _n: 'hello', _a: [], _o: 'string', _t: [], _d: '' };
+    it("no function", () => {
+        expect(validateFunction({})).to.deep.equal(['error', ['missing name', 'missing output', 'missing arguments', 'missing tags', 'missing description']]);
     });
+    it("simple function", () => {
+        expect(validateFunction(hello)).to.deep.equal(['ok', hello]);
+    });
+    it("simple function - lower camel case error", () => {
+        var hello2 = hello;
+        hello2._n = 'Hello';
+        expect(validateFunction(hello2)).to.deep.equal(['error', ['name must be lower camel case']]);
+    });
+
 });
