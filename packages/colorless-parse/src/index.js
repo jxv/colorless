edf527a80777ce87dafdbf17868974c8a602f903
jxv <joevargas92@gmail.com>
2017-02-14 23:25:27 -0800

    remove underscores as special key markers

--- packages/colorless-parse/src/index.js
+++ packages/colorless-parse/src/index.js
@@ -1,13 +1,13 @@
 /*
- * _n = name
- * _f = fields
- * _c = constructors
- * _s = type synonym
- * _a = arguments
- * _o = output type
- * _p = type parameters
- * _t = tags
- * _k = type level value constraint
+ * n = name
+ * f = fields
+ * c = constructors
+ * s = type synonym
+ * a = arguments
+ * o = output type
+ * p = type parameters
+ * t = tags
+ * k = type level value constraint
  *
  */
 
@@ -26,96 +26,96 @@ function notKeys(ls, o) {
 }
 
 function isSum(v) {
-    return hasKeys(['_n', '_c'], v) && notKeys(['_f', '_a', '_s', '_o'], v);
+    return hasKeys(['n', 'c'], v) && notKeys(['f', 'a', 's', 'o'], v);
 }
 
 function Sum(name, params, ctors, tags, description) {
-    this._n = name;
-    this._p = params || [];
-    this._c = ctors;
-    this._t = tags || [];
-    this._d = description || '';
+    this.n = name;
+    this.p = params || [];
+    this.c = ctors;
+    this.t = tags || [];
+    this.d = description || '';
 }
 
 function toSum(v) {
-    return new Sum(v._n, v._p, v._c, v._t, v._d);
+    return new Sum(v.n, v.p, v.c, v.t, v.d);
 }
 
 function isConstructor(v) {
-    return hasKeys(['_n'], v) && notKeys(['_f', '_a', '_c', '_s', '_o', '_t'], v);
+    return hasKeys(['n'], v) && notKeys(['f', 'a', 'c', 's', 'o', 't'], v);
 }
 
 function Constructor(name, params) {
-    this._n = name;
-    this._p = params || [];
+    this.n = name;
+    this.p = params || [];
 }
 
 function toConstructor(v) {
-    return new Constructor(v._n, v._p);
+    return new Constructor(v.n, v.p);
 }
 
 function isProduct(v) {
-    return hasKeys(['_n', '_f'], v) && notKeys(['_c', '_a', '_s', '_o'], v);
+    return hasKeys(['n', 'f'], v) && notKeys(['c', 'a', 's', 'o'], v);
 }
 
 function Product(name, params, fields, tags, description) {
-    this._n = name;
-    this._p = params || [];
-    this._f = fields;
-    this._t = tags || [];
-    this._d = description || '';
+    this.n = name;
+    this.p = params || [];
+    this.f = fields;
+    this.t = tags || [];
+    this.d = description || '';
 }
 
 function toProduct(v) {
-    return new Product(v._n, v._p, v._f, v._t, v._d);
+    return new Product(v.n, v.p, v.f, v.t, v.d);
 }
 
 function isSynonym(v) {
-    return hasKeys(['_n', '_s'], v) && notKeys(['_c', '_f', '_a', '_o'], v);
+    return hasKeys(['n', 's'], v) && notKeys(['c', 'f', 'a', 'o'], v);
 }
 
 function Synonym(name, params, synonym, tags, description) {
-    this._n = name;
-    this._p = params || [];
-    this._s = synonym;
-    this._t = tags || [];
-    this._d = description || '';
+    this.n = name;
+    this.p = params || [];
+    this.s = synonym;
+    this.t = tags || [];
+    this.d = description || '';
 }
 
 function toSynonym(v) {
-    return new Synonym(v._n, v._p, v._s, v._t, v._d);
+    return new Synonym(v.n, v.p, v.s, v.t, v.d);
 }
 
 var types = {
-    "_types": [
-        { "_n": "Suit", "_c": [ { "_n": "Hearts" }, { "_n": "Diamonds" }, { "_n": "Clubs" }, { "_n": "Spades" }] },
-        { "_n": "Rank", "_c": [
-                { "_n": "Ace" },
-                { "_n": "R2" },
-                { "_n": "R3" },
-                { "_n": "R4" },
-                { "_n": "R5" },
-                { "_n": "R6" },
-                { "_n": "R7" },
-                { "_n": "R8" },
-                { "_n": "R9" },
-                { "_n": "R10" },
-                { "_n": "Jack" },
-                { "_n": "Queen" },
-                { "_n": "King" }
+    "types": [
+        { "n": "Suit", "c": [ { "n": "Hearts" }, { "n": "Diamonds" }, { "n": "Clubs" }, { "n": "Spades" }] },
+        { "n": "Rank", "c": [
+                { "n": "Ace" },
+                { "n": "R2" },
+                { "n": "R3" },
+                { "n": "R4" },
+                { "n": "R5" },
+                { "n": "R6" },
+                { "n": "R7" },
+                { "n": "R8" },
+                { "n": "R9" },
+                { "n": "R10" },
+                { "n": "Jack" },
+                { "n": "Queen" },
+                { "n": "King" }
             ]
         },
-        { "_n": "A", "_f": { "x": { "_n": "i32" } }, "_t": [ "C" ] },
-        { "_n": "A", "_f": { "x": { "_n": "i32" } }, "_t": [ "C" ] },
-        { "_n": "B", "_p": [ { "_n": "a" } ], "_f": { "x": "a" }, "_t": [ "C" ] },
-        { "_n": "D", "_c": [ { "_n": "X" }, { "_n": "Y" }, { "_n": "Z" } ] },
-        { "_n": "E", "_p": [ { "_n": "a" }, { "_n": "b" }], "_c": [
-                { "_n": "X", "_p": [ { "_n": "List", "_p": [ {"_n": "a" } ] } ] },
-                { "_n": "Y", "_p": [ { "_n": "List", "_p": [ {"_n": "b" } ] } ] },
-                { "_n": "Z" }
+        { "n": "A", "f": { "x": { "n": "i32" } }, "t": [ "C" ] },
+        { "n": "A", "f": { "x": { "n": "i32" } }, "t": [ "C" ] },
+        { "n": "B", "p": [ { "n": "a" } ], "f": { "x": "a" }, "t": [ "C" ] },
+        { "n": "D", "c": [ { "n": "X" }, { "n": "Y" }, { "n": "Z" } ] },
+        { "n": "E", "p": [ { "n": "a" }, { "n": "b" }], "c": [
+                { "n": "X", "p": [ { "n": "List", "p": [ {"n": "a" } ] } ] },
+                { "n": "Y", "p": [ { "n": "List", "p": [ {"n": "b" } ] } ] },
+                { "n": "Z" }
             ] 
         },
-        { "_n": "Id", "_p": [ { "_n": "a", "_k": "string" } ], "_s": { "_n": "i32" } }
+        { "n": "Id", "p": [ { "n": "a", "k": "string" } ], "s": { "n": "i32" } }
     ]
 };
 
@@ -126,24 +126,24 @@ function typeDeclTag(v) {
 }
 
 function typeDeclTags(v) {
-    return v._types.map(function (t) { return typeDeclTag(t); } );
+    return v.types.map(function (t) { return typeDeclTag(t); } );
 }
 
 //
 
 var functions = {
-    "_functions": [
-        { "_n": "helloWorld", "_o": "string" },
-        { "_n": "add", "_a": [["x","f32"], ["y","f32"]], "_o": "f32" }
+    "functions": [
+        { "n": "helloWorld", "o": "string" },
+        { "n": "add", "a": [["x","f32"], ["y","f32"]], "o": "f32" }
     ]
 };
 
 function isFunctionDecl(f) {
-    return '_n' in f && '_o' in f
+    return 'n' in f && 'o' in f
 }
 
 function areFunctionDecls(v) {
-    return v._functions.map(function (f) { return [f._n, isFunctionDecl(f)]; } );
+    return v.functions.map(function (f) { return [f.n, isFunctionDecl(f)]; } );
 }
 
 function hasCamelCaseCharacters(s) {
@@ -182,18 +182,18 @@ Errors.prototype = {
 
 function validateFunction(f) {
     var errors = new Errors();
-    errors.assert('_n' in f, 'missing name') &&
-        errors.assert(isLowerCamelCase(f._n), 'lower camel case name');
-    errors.assert('_o' in f, 'missing output') &&
-        errors.assert(isPrimitiveType(f._o), 'invalid primitive output');
-    errors.assert('_a' in f, 'missing arguments');
-    errors.assert('_t' in f, 'missing tags');
-    errors.assert('_d' in f, 'missing description');
+    errors.assert('n' in f, 'missing name') &&
+        errors.assert(isLowerCamelCase(f.n), 'lower camel case name');
+    errors.assert('o' in f, 'missing output') &&
+        errors.assert(isPrimitiveType(f.o), 'invalid primitive output');
+    errors.assert('a' in f, 'missing arguments');
+    errors.assert('t' in f, 'missing tags');
+    errors.assert('d' in f, 'missing description');
     return errors.orResult(() => f);
 }
 
 var domains = {
-    "_domains": [
+    "domains": [
         {
             "name": "Calculator",
             "modules": [
@@ -225,7 +225,7 @@ var domains = {
 };
 
 var services = {
-    "_services": [
+    "services": [
         [   "http",
             {
                 "name": "Root",
@@ -234,7 +234,7 @@ var services = {
                 "format": "json",
                 "port": 8888,
                 "domain": "Combined",
-                "error": { "_n": "Error" }
+                "error": { "n": "Error" }
             }
         ],
         [   
@@ -246,7 +246,7 @@ var services = {
                 "format": "json",
                 "port": 8888,
                 "domain": "Calculator",
-                "error": { "_n": "unit" }
+                "error": { "n": "unit" }
             }
         ],
         [ 
@@ -258,7 +258,7 @@ var services = {
                 "format": "json",
                 "port": 8888,
                 "domain": "Example",
-                "error": { "_n": "unit" }
+                "error": { "n": "unit" }
             }
         ]
     ]
--- packages/colorless-parse/test/parseSpec.js
+++ packages/colorless-parse/test/parseSpec.js
@@ -5,7 +5,7 @@ const expect = require('chai').expect;
 const { validateFunction, Error,  Ok } = require('../src/index.js');
 
 describe("parsing validation:", () => {
-    const hello = { _n: 'hello', _a: [], _o: 'string', _t: [], _d: '' };
+    const hello = { n: 'hello', a: [], o: 'string', t: [], d: '' };
     it("no function", () => {
         expect(validateFunction({})).to.deep.equal(['error', ['missing name', 'missing output', 'missing arguments', 'missing tags', 'missing description']]);
     });
@@ -14,12 +14,12 @@ describe("parsing validation:", () => {
     });
     it("simple function - lower camel case error", () => {
         var hello2 = R.clone(hello);
-        hello2._n = 'Hello';
+        hello2.n = 'Hello';
         expect(validateFunction(hello2)).to.deep.equal(['error', ['lower camel case name']]);
     });
     it("simple function - invalid primitive output error", () => {
         var hello2 = R.clone(hello);
-        hello2._o = 'asdf';
+        hello2.o = 'asdf';
         expect(validateFunction(hello2)).to.deep.equal(['error', ['invalid primitive output']]);
     });
 });
