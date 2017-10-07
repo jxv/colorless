fe4267b53c87dec6a40fca4c3e405a01ca232030
jxv <joevargas92@gmail.com>
2017-10-06 22:16:06 -0700

    examples: move specs into separate 'specs' directory

--- examples/gen.sh
+++ examples/gen.sh
@@ -5,13 +5,12 @@ colorless -l haskell -s HelloWorldPublic/specs.json -m HelloWorld -n HelloWorld 
 colorless -l haskell -s HelloWorldPublic/specs.json -m HelloWorld -n HelloWorld -d ./HelloWorldPublic/haskell-client -e client -a http-client
 colorless -l javascript -s HelloWorldPublic/specs.json -n helloWorld -d ./HelloWorldPublic/javascript-client -e client
 
-# HelloWorl with Public spec
-colorless -l haskell -s HelloWorld -m HelloWorld -n HelloWorld -d ./HelloWorld/haskell-server -e server -a scotty
-colorless -l haskell -s HelloWorld -m HelloWorld -n HelloWorld -d ./HelloWorld/haskell-client -e client -a http-client
-colorless -l javascript -s HelloWorld -n helloWorld -d ./HelloWorld/javascript-client -e client
-
+# HelloWorl with public specs
+colorless -l haskell -s HelloWorld/specs -m HelloWorld -n HelloWorld -d ./HelloWorld/haskell-server -e server -a scotty
+colorless -l haskell -s HelloWorld/specs -m HelloWorld -n HelloWorld -d ./HelloWorld/haskell-client -e client -a http-client
+colorless -l javascript -s HelloWorld/specs -n helloWorld -d ./HelloWorld/javascript-client -e client
 
 # Phonebook
-colorless -l haskell -s Phonebook -m Phonebook -n Phonebook -d ./Phonebook/haskell-server -e server -a scotty
-colorless -l haskell -s Phonebook -m Phonebook -n Phonebook -d ./Phonebook/haskell-client -e client -a http-client
-colorless -l javascript -s Phonebook -n phonebook -d ./Phonebook/javascript-client -e client
+colorless -l haskell -s Phonebook/specs -m Phonebook -n Phonebook -d ./Phonebook/haskell-server -e server -a scotty
+colorless -l haskell -s Phonebook/specs -m Phonebook -n Phonebook -d ./Phonebook/haskell-client -e client -a http-client
+colorless -l javascript -s Phonebook/specs -n phonebook -d ./Phonebook/javascript-client -e client
