59423c028cb85c37d70ea23d6a3b9727425b473b
jxv <joevargas92@gmail.com>
2017-12-12 13:13:28 -0800

    applyVersionFromSpec(s)

--- fluid-cli/src/Fluid/Gen/Version.purs
+++ fluid-cli/src/Fluid/Gen/Version.purs
@@ -1,4 +1,7 @@
-module Fluid.Gen.Version where
+module Fluid.Gen.Version
+  ( applyVersionsFromSpecs
+  , applyVersionsFromSpec
+  ) where
 
 import Prelude
 
@@ -6,27 +9,37 @@ import Data.Array as Array
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
-import Data.Path.Pathy (FileName(..), extension)
 import Data.StrMap as StrMap
-import Data.StrMap (StrMap)
 import Data.Traversable (traverse)
-import Fluid.Gen.Diff (Diff, diffs)
-import Fluid.Gen.Spec (parseSpecs, Spec, TypeName, Version, Schema, Bridge)
+import Fluid.Gen.Spec (Version)
 import Data.Argonaut.Core (Json)
 import Data.Argonaut.Core as A
 import Data.Argonaut.Decode as A
-import Data.Argonaut.Encode as A
+import Data.Argonaut.Encode.Class as A
 import Data.Argonaut.Parser as A
 
-applyVersionsOnSpecs :: Array (Tuple Version String) -> Either String (Tuple String (Array String))
-applyVersionsOnSpecs pairs = do
+applyVersionsFromSpecs :: Array (Tuple Version String) -> Either String (Tuple String (Array String))
+applyVersionsFromSpecs pairs = do
   specs <- traverse applyVersion pairs
   pure $ Tuple (A.stringify $ A.encodeJson specs) (map A.stringify specs)
+
+applyVersionsFromSpec :: Array Version -> String -> Either String (Tuple String (Array String))
+applyVersionsFromSpec versions spec = do
+  spec' <- A.jsonParser spec
+  case A.toArray spec' of
+    Nothing -> Left "Not an array of specs"
+    Just specs -> do
+      jsons <- traverse (\(Tuple v s) -> applyVersionToJson v s) (Array.zip versions specs)
+      pure $ Tuple (A.stringify $ A.fromArray jsons) (map A.stringify jsons)
 
 applyVersion :: Tuple Version String -> Either String Json
 applyVersion (Tuple version spec) = do
   spec' <- A.jsonParser spec
-  obj <- A.decodeJson spec'
+  applyVersionToJson version spec'
+
+applyVersionToJson :: Version -> Json -> Either String Json
+applyVersionToJson version spec = do
+  obj <- A.decodeJson spec
   let obj' = StrMap.insert "version" (encodeVersion version) obj
   pure $ A.encodeJson obj'
   where
