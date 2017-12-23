b522b9f7cac8359ee057dd2a53215a2f793dd99c
jxv <joevargas92@gmail.com>
2017-12-22 19:09:17 -0800

    gen hs client (init) in purs

--- fluid-cli/src/Fluid/Gen/Haskell/ServerVersion.purs
+++ fluid-cli/src/Fluid/Gen/Haskell/ServerVersion.purs
@@ -4,7 +4,7 @@ import Data.Array as Array
 import Data.Foldable (sequence_)
 import Data.Maybe (Maybe(..), isJust)
 import Data.Traversable (traverse_)
-import Fluid.Gen.Haskell.Common (enumeralNameTagMember, genPragmas, mkExportTypes)
+import Fluid.Gen.Haskell.Common
 import Fluid.Gen.Haskell.Spec (Enumeral, Enumeration, Func, Plan, Struct, Wrap, lowercaseFirstLetter, uppercaseFirstLetter, PullPlan)
 import Fluid.Gen.Lines (Lines, addLine, line, lines, linesContent, lineList)
 import Fluid.Gen.Spec (Version)
@@ -20,159 +20,10 @@ mkImportTypes plan = Array.filter (\a -> a.major /= plan.version.major) $
       Array.catMaybes (map (enumeralImport e.name e.major) e.enumerals))
     plan.enumerations
 
-memberName :: String -> String -> String
-memberName name member = lowercaseFirstLetter name <> uppercaseFirstLetter member
-
 enumeralImport :: String -> Int -> Enumeral -> Maybe { name :: String, major :: Int }
 enumeralImport name major enumeral = case enumeral.members of
   Nothing -> Nothing
   Just _ -> Just { name: enumeralNameTagMember name enumeral.tag, major }
-
-genToJson :: forall a. { name :: String | a } -> Lines Unit
-genToJson {name} = do
-  line ""
-  addLine ["instance R.ToJSON ", name, " where"]
-  line "  toJSON = R.toJSON P.. C.toVal"
-
-genFromJson :: forall a. { name :: String | a } -> Lines Unit
-genFromJson {name} = do
-  line ""
-  addLine ["instance R.FromJSON ", name, " where"]
-  lines
-    [ "  parseJSON _v = do"
-    , "    _x <- R.parseJSON _v"
-    , "    case C.fromVal _x of"
-    , "      P.Nothing -> P.mzero"
-    , "      P.Just _y -> P.return _y" ]
-
-genWrap :: Wrap -> Lines Unit
-genWrap {name, type: type', label, instances: {text, number}} = do
-  line ""
-  addLine ["-- Wrap: ", name]
-  addLine ["newtype ", name, " = ", name, " ", type']
-  addLine ["  deriving (P.Eq, P.Ord, ", if text then "P.IsString, R.ToText, " else "", if number then "P.Num, " else "", " P.Show)"]
-
-genWrapToVal :: Wrap -> Lines Unit
-genWrapToVal {name} = do
-  line ""
-  addLine ["instance C.ToVal ", name, " where"]
-  addLine ["  toVal (", name, " _w) = C.toVal _w"]
-
-genWrapFromVal :: Wrap -> Lines Unit
-genWrapFromVal {name} = do
-  line ""
-  addLine ["instance C.FromVal ", name, " where"]
-  addLine ["  fromVal _v = ", name, " P.<$> C.fromVal _v"]
-
-genStruct :: Struct -> Lines Unit
-genStruct {name, label, members} = do
-  line ""
-  addLine ["-- Struct: ", name]
-  addLine ["data ", name, " = ", name]
-  lineList members
-    "  { "
-    "  , "
-    (\m -> [lowercaseFirstLetter name <> uppercaseFirstLetter m.name, " :: ", m.type])
-  line "  } deriving (P.Show, P.Eq)"
-
-genStructToVal :: Struct -> Lines Unit
-genStructToVal {name, members} = do
-  line ""
-  addLine ["instance C.ToVal ", name, " where"]
-  addLine ["  toVal ", name]
-  lineList members
-    "    { "
-    "    , "
-    (\m -> [memberName name m.name])
-  line "    } = C.Val'ApiVal P.$ C.ApiVal'Struct P.$ C.Struct P.$ R.fromList"
-  lineList members
-    "    [ "
-    "    , "
-    (\m -> ["(\"", m.label, "\", C.toVal ", memberName name m.name, ")"])
-  line "    ]"
-  line ""
-
-genStructFromVal :: Struct -> Lines Unit
-genStructFromVal {name,members} = do
-  addLine ["instance C.FromVal ", name, " where"]
-  line "  fromVal = \\case"
-  addLine ["    C.Val'ApiVal (C.ApiVal'Struct (C.Struct _m)) -> ", name]
-  lineList members
-    "      P.<$>"
-    "      P.<*>"
-    (\m -> [" C.getMember _m \"", m.label, "\""])
-  line "    _ -> P.Nothing"
-
-genEnumeration :: Enumeration -> Lines Unit
-genEnumeration {name, enumerals} = do
-  line ""
-  addLine ["-- Enumeration: ", name]
-  addLine ["data ", name]
-  lineList enumerals "  = " "  | " (\e -> [name, "'", e.tag, if isJust e.members then " " <> name <> "'" <> e.tag <> "'Members" else ""])
-  addLine ["  deriving (P.Show, P.Eq)"]
-  flip traverse_ enumerals $ \enumeral ->
-    case enumeral.members of
-      Nothing -> pure unit
-      Just members -> do
-        line ""
-        addLine ["data ", name, "'", enumeral.tag, "'Members = ", name, "'", enumeral.tag, "'Members"]
-        lineList members
-          "  { "
-          "  , "
-          (\m -> [lowercaseFirstLetter(name <> "'" <> enumeral.tag <> uppercaseFirstLetter m.name), " :: ", m.type])
-        line "  } deriving (P.Show, P.Eq)"
-
-genEnumerationToVal :: Enumeration -> Lines Unit
-genEnumerationToVal {name,enumerals} = do
-  line ""
-  addLine ["instance C.ToVal ", name, " where"]
-  line "  toVal = \\case"
-  flip traverse_ enumerals $ \enumeral -> case enumeral.members of
-    Nothing -> addLine ["    ", name, "'", enumeral.tag, " -> C.Val\'ApiVal P.$ C.ApiVal\'Enumeral P.$ C.Enumeral \"", enumeral.label, "\" P.Nothing"]
-    Just members -> do
-      addLine ["    ", name, "'", enumeral.tag, " ", name, "'", enumeral.tag, "'Members"]
-      lineList members
-        "      { "
-        "      , "
-        (\m -> [memberName (name <> "'" <> enumeral.tag) m.name])
-      addLine ["      } -> C.Val\'ApiVal P.$ C.ApiVal\'Enumeral P.$ C.Enumeral \"", enumeral.label, "\" P.$ P.Just P.$ R.fromList"]
-      lineList members
-        "      [ "
-        "      , "
-        (\m -> ["(\"", m.label, "\", C.toVal ", memberName (name <> "'" <> enumeral.tag) m.name, ")"])
-      line "      ]"
-
-genEnumerationFromVal :: Enumeration -> Lines Unit
-genEnumerationFromVal {name,enumerals} = do
-  line ""
-  addLine ["instance C.FromVal ", name, " where"]
-  line "  fromVal = \\case"
-  line "    C.Val'ApiVal (C.ApiVal'Enumeral (C.Enumeral _tag _m)) -> case (_tag,_m) of"
-  flip traverse_ enumerals $ \enumeral -> case enumeral.members of
-    Nothing -> addLine ["      (\"", enumeral.label, "\", P.Nothing) -> P.Just ", name, "'", enumeral.tag]
-    Just members -> do
-      addLine ["      (\"", enumeral.label, "\", P.Just _m') -> ", name, "'", enumeral.tag, " P.<$> (", name, "'", enumeral.tag, "'Members"]
-      lineList members
-        "          P.<$>"
-        "          P.<*>"
-        (\m -> [" C.getMember _m' \"", m.label, "\""])
-      line "        )"
-  line "      _ -> P.Nothing"
-  line "    _ -> P.Nothing"
-
-genVersion :: { lowercase :: String, version :: Version } -> Lines Unit
-genVersion {lowercase, version} = do
-  line ""
-  line "-- Version"
-  addLine [lowercase, "'version :: C.Version"]
-  addLine [lowercase, "'version = C.Version ", show version.major, " ", show version.minor]
-
-genPull :: { lowercase :: String, pull :: PullPlan } -> Lines Unit
-genPull {lowercase, pull: {protocol, host, path, port}} = do
-  line ""
-  addLine [lowercase, "'pull :: C.Pull"]
-  addLine [lowercase, "'pull = C.Pull \"", protocol, "\" \"", host, "\" \"", path, "\" ", show port]
-
 
 genImports :: { prefix :: String, imports :: Array { name :: String, major :: Int }, importing :: Array (Lines Unit) } -> Lines Unit
 genImports {prefix, imports, importing} = do
@@ -261,9 +112,6 @@ mkApiParserCalls plan =
   where
     f :: forall a. { name :: String, label :: String | a } -> { name :: String, label :: String }
     f ty = { name: ty.name, label: ty.label }
-
-filterFunc :: forall a. Array { func :: Maybe Func | a } -> Array { func :: Maybe Func | a}
-filterFunc = Array.filter (\{func} -> isJust func)
 
 genApiParser
   :: { name :: String
