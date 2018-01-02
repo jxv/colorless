1779344cac7e2ada66ba1e85c24da71702846ac3
jxv <joevargas92@gmail.com>
2018-01-02 08:20:31 -0800

    rust

--- fluid-cli/src/Fluid/Gen/Rust/Common.purs
+++ fluid-cli/src/Fluid/Gen/Rust/Common.purs
@@ -1,1 +1,45 @@
-module Fuid.Gen.Rust.Common where
+module Fluid.Gen.Rust.Common where
+
+import Prelude
+import Data.Array as Array
+import Data.Maybe (Maybe(..), isJust)
+import Data.Traversable (traverse_)
+import Fluid.Gen.Spec (Version)
+import Fluid.Gen.Lines
+import Fluid.Gen.Plan
+
+genWrap :: Wrap -> Lines Unit
+genWrap {name, type: type', label, instances: {text, number}} = do
+  line ""
+  addLine ["// Wrap: ", name]
+  line "#[derive(Debug)]"
+  addLine ["struct ", name, "(", type',");"]
+
+genStruct :: Struct -> Lines Unit
+genStruct {name, label, members} = do
+  line ""
+  addLine ["// Struct: ", name]
+  line "#[derive(Debug)]"
+  addLine ["struct ", name, " {"]
+  flip traverse_ members $ \member ->
+    addLine ["    ", member.name, ": ", member.type, ","]
+  line "}"
+
+genEnumeration :: Enumeration -> Lines Unit
+genEnumeration {name, enumerals} = do
+  line ""
+  addLine ["// Enumeration: ", name]
+  line "#[derive(Debug)]"
+  addLine ["enum ", name, " {"]
+
+  flip traverse_ enumerals $ \enumeral ->
+    addLine $
+      ["    ", enumeral.tag] <>
+      (case enumeral.members of
+        Nothing -> [""]
+        Just members ->
+          [" { "] <>
+          (flip Array.concatMap members $ \member -> [member.name, ": ", member.type, ","]) <>
+          [" }"]
+          ) <>
+      [","]
--- fluid-cli/src/Fluid/Gen/Rust/ServerVersion.purs
+++ fluid-cli/src/Fluid/Gen/Rust/ServerVersion.purs
@@ -1,8 +1,30 @@
 module Fluid.Gen.Rust.ServerVersion where
 
-import Fluid.Gen.Plan (Plan)
+import Prelude (discard, (==))
+import Data.Array as Array
+import Data.Traversable (traverse_)
+
+import Fluid.Gen.Plan (Enumeral, Enumeration, Func, Plan, Struct, Wrap, lowercaseFirstLetter, uppercaseFirstLetter, PullPlan)
+import Fluid.Gen.Spec (Version)
 import Fluid.Gen.Lines (linesContent, line)
+
+import Fluid.Gen.Rust.Common
+
+filterVersion :: forall a. Version -> Array { major :: Int | a } -> Array { major :: Int | a }
+filterVersion version = Array.filter (\ty -> ty.major == version.major)
 
 gen :: Plan -> Array String -> String
 gen plan addonNames = linesContent do
+
+  let currentWraps = filterVersion plan.version plan.wraps
+  let currentStructs = filterVersion plan.version plan.structs
+  let currentEnumerations = filterVersion plan.version plan.enumerations
+
   line ""
+  line "////////////////////////////////////////////////////////"
+  line "// Types"
+  line "////////////////////////////////////////////////////////"
+  line ""
+  traverse_ genWrap currentWraps
+  traverse_ genStruct currentStructs
+  traverse_ genEnumeration currentEnumerations
